8c740d79f064bdaccdba4a1e918ac0d36253d193
37coHiNa <37cohina@gmail.com>
2020-11-25 17:40:38 +0900

    Update main.js

--- practice/3D/js/main.js
+++ practice/3D/js/main.js
@@ -1,21 +1,18 @@
 
 import * as THREE from "../lib/three.module.js";
 
-const canvas = Object.assign(
-  document.createElement( "canvas" ),
+const canvas = Object.assign( document.createElement( "canvas" ),
   {
     width: 500,
     height: 500
   }
 );
 
-const contentArea = Object.assign(
-  document.createElement( "div" ),
+const contentArea = Object.assign( document.createElement( "div" ),
   {
   }
 );
-Object.assign(
-  contentArea.style,
+Object.assign( contentArea.style,
   {
     textAlign: "center"
   }
@@ -23,18 +20,71 @@ Object.assign(
 
 document.body.appendChild( contentArea ).appendChild( canvas );
 
-const form = Object.assign(
-  document.createElement( "form" ),
+const formData = {
+  camera: {
+    position: {
+      x: 0,
+      y: 0,
+      z: 5
+    }
+  },
+  vertices: [
+  ]
+};
+
+const form = Object.assign( document.createElement( "form" ),
   {
   }
 );
 
+function vertices_onChange( event ) {
+  
+  const index = [ ...form.querySelectorAll( `[name="vertices"]` ) ].indexOf( event.target );
+  
+  if ( index == -1 ) {
+    
+    console.error( event );
+    return;
+    
+  }
+  
+  vertices[ index ] = Number( event.target.value ) || 0.0;
+  
+}
+
+function addRow_vertices( x = 0.0, y = 0.0, z = 0.0 ) {
+  
+  const row = form.appendChild( document.createElement( "p" ) );
+  
+  for ( const value of [ x, y, z ] ) {
+    
+    Object.assign( row.appendChild( document.createElement( "input" ) ),
+      {
+        name: "vertices",
+        value
+      }
+    ).addEventListener( "change", vertices_onChange );
+    
+    formData.vertices.push( value );
+    
+  }
+  
+  return row;
+  
+}
+
+for ( let i = 0; i < 3; i++ ) {
+  
+  addRow_vertices();
+  
+}
+
 document.body.appendChild( form );
+
+const renderer = new THREE.WebGLRenderer( { canvas } );
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, canvas.width / canvas.height, 0.1, 1000 );
-camera.position.set( 0, 0, 5 );
-const renderer = new THREE.WebGLRenderer( { canvas } );
 
 const geometry = new THREE.BufferGeometry();
 const material = new THREE.MeshBasicMaterial();
@@ -43,18 +93,9 @@ scene.add( mesh );
 
 const animate = function () {
   requestAnimationFrame( animate );
+  camera.position.set( formData.camera.position.x, formData.camera.position.y, formData.camera.position.z );
   material.color.setRGB( 0.596078431372549, 0, 0 );
-  const vertices = new Float32Array( [ ...(function*(){
-    for ( const z of [ 1.0, -1.0 ] ) {
-      const prams = [
-      -1.0, -1.0, z,
-       1.0, -1.0, z,
-       1.0,  1.0, z,
-     ];
-     for ( const param of prams ) yield param;
-    }
-  })() ] );
-  geometry.setAttribute( "position", new THREE.BufferAttribute( vertices, 3 ) );
+  geometry.setAttribute( "position", new THREE.BufferAttribute( new Float32Array( formData.vertices );, 3 ) );
   mesh.rotation.x += 0.01;
   mesh.rotation.y += 0.01;
   renderer.render( scene, camera );
