b010ead0482d454f5fe8822b55a1db494fc8724d
37coHiNa <37cohina@gmail.com>
2020-11-25 16:58:22 +0900

    Update main.js

--- practice/3D/js/main.js
+++ practice/3D/js/main.js
@@ -33,7 +33,7 @@ document.body.appendChild( form );
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, canvas.width / canvas.height, 0.1, 1000 );
-camera.position = new THREE.Vector3( 0, 0, 5 );
+camera.position.set( 0, 0, 5 );
 const renderer = new THREE.WebGLRenderer( { canvas } );
 
 const geometry = new THREE.BufferGeometry();
