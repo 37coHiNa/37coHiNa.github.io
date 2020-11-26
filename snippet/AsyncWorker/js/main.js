232f1e7f4ce787977b33ac1d15549f80a082a153
37coHiNa <37cohina@gmail.com>
2020-11-27 07:47:56 +0900

    Update main.js

--- snippet/AsyncWorker/js/main.js
+++ snippet/AsyncWorker/js/main.js
@@ -13,7 +13,7 @@ class AsyncWorker extends Worker {
       const { requestID, index, message, status } = event.data;
       console.log( `requestID=${ requestID }, status=${ status }, index=${ index }, message=${ message }` );
       
-      const request = requests.get( requestID );
+      const request = this.#equests.get( requestID );
       request.set( index, { message, status } );
       
     } );
