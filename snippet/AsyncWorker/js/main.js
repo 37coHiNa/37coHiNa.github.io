313de9e30fa0a67450f95b3cfc8d5c0970aaa076
37coHiNa <37cohina@gmail.com>
2020-11-26 14:21:43 +0900

    Update main.js

--- snippet/AsyncWorker/js/main.js
+++ snippet/AsyncWorker/js/main.js
@@ -1,69 +1,3 @@
-class Request extends EventTarget {
-  
-  #index = 0;
-  #length = null;
-  #status = "";
-  #cache = new Map();
-  
-  constructor() {
-    super();
-  }
-  
-  get index() { return this.#index; }
-  
-  get length() { return this.#length; }
-  
-  get status() { return this.#status; }
-                
-  get cache() { return this.#cache; }
-                
-  #response( index, message, status ) {
-  
-    if ( status != "" ) {
-      
-      if ( this.#status != "" ) {
-          
-          throw new TypeError();
-      
-      }
-      
-      this.#status = status;
-      this.#length = index;
-      
-    } else if ( this.#index == index ) {
-        
-      for ( let m = message; this.#cache.has( this.#index ) ; ) {
-        
-        const data = { index: this.#index, message: m };
-        this.dispatchEvent( new MessageEvent( "response", { data } ) );
-        this.#index++;
-        m = this.#cache.get( this.#index );
-          
-      }
-        
-      if ( this.#status != "" && this.#length <= this.#index ) {
-        
-        const data = { status: this.#status, length: this.#length };
-        this.dispatchEvent( new MessageEvent( "close", { data } ) );
-          
-      }
-        
-    } else {
-        
-      this.#cache.set( index, message );
-        
-    }
-  
-  }
-
-  get response() {
-    
-    return this.#response;
-    
-  }
-  
-}
-
 class AsyncWorker extends Worker {
   
   #requests = new Map();
@@ -80,11 +14,7 @@ class AsyncWorker extends Worker {
       console.log( `requestID=${ requestID }, status=${ status }, index=${ index }, message=${ message }` );
       
       const request = requests.get( requestID );
-      if ( request ) {
-        
-        request.response( message );
-        
-      }
+      request.set( index, { message, status } );
       
     } );
     
@@ -96,63 +26,52 @@ class AsyncWorker extends Worker {
     
   }
   
-  async postMessage( ...args ) {
+  postMessage( ...args ) {
     
     const requestID = this.#createRequestID();
     const requests = this.#requests;
-    const request = new Request();
-    
-    const responseCache = [];
-    let count = 0;
-      
-    request.addEventListener( "response", event => {
-        
-      responseCache.push( event.data.message );
-      count++;
-        
-    } );
-      
-    let status = "";
-    let error = null;
-    let length = null;
-    request.addEventListener( "close", event => {
-          
-      status = event.data.status;
-      error = event.data.message;
-      length = event.data.length;
-
-    } );
-      
+    const request = new Map();
     requests.set( requestID, request );
-      
+    
     super.postMessage( { requestID, args } );
     
     return ( async function* () {
       
+      let index = 0;
+      
       while (1) {
         
-        const next = new Promise( resolve => {
+        const { message, status } = await new Promise( resolve => {
         
           (function _(){
           
-            if ( responseCache.length > 0 ) resolve( responseCache.shift() );
-            else setTimeout( _, 0 );
+            if ( request.has( index ) ) {
+              
+              const data = request.get( index );
+              request.delete( index );
+              console.log( index, data );
+              index++;
+              resolve( data );
+              
+            } else {
+              
+              setTimeout( _, 0 );
+              
+            }
         
           })();
         
         } );
         
-        console.log( next );
-        
-        yield await next;
-        
         if ( status != "" ) {
           
-          console.log( `status=${ status }, count=${ count }, length=${ length }` );
+          console.log( `status=${ status }, length=${ index }` );
           
           return;
-          
+        
         }
+        
+        yield message;
         
       }
       
