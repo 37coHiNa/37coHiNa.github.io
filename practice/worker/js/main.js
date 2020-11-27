b777d38060bcd8a4af204531fdd0d6b66f6e3b18
37coHiNa <37cohina@gmail.com>
2020-11-27 23:51:56 +0900

    Update main.js

--- practice/worker/js/main.js
+++ practice/worker/js/main.js
@@ -1,5 +1,11 @@
 import * as PromisedWorker from "../js/lib/PromisedWorker.js";
 
+PromisedWorker.methods.test = function( request ) {
+  
+  console.log( "!ERROR!" );
+  
+};
+
 const worker = new PromisedWorker.Worker( "./js/worker.js", { type: "module" } );
 (async ()=>{
   for await ( const message of worker.postMessage( "main", "test message", "test message2", 123 ) ) {
