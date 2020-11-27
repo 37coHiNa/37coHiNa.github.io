16a5ea7235bc257dfff0c0b8c665c6212fa8ff36
37coHiNa <37cohina@gmail.com>
2020-11-27 16:07:00 +0900

    Update worker.js

--- snippet/AsyncWorker/js/worker.js
+++ snippet/AsyncWorker/js/worker.js
@@ -1,98 +1,11 @@
-class WorkerRequest {
+import Action from "./js/lib/PromiseWorker.js";
 
-  #requestID;
-  #args;
-  #status = ""
-  #index = 0;
-  
-  constructor ( { requestID, args } ) {
-    
-    console.log( requestID, args );
-  
-    this.#requestID = requestID;
-    this.#args = args;
-    
-  }
-  
-  get args() {
-    
-    return this.#args;
-    
-  }
-  
-  #postMessage( message ) {
-  
-    const requestID = this.#requestID;
-    const status = this.#status;
-    const index = this.#index;
-    
-    this.#index++;
-    return self.postMessage( { requestID, message, index, status } );
-    
-  }
-  
-  #responseStatus( status, message ) {
-    
-    if ( this.#status != "" ) {
-    
-      throw new TypeError( `status: ${ this.#status }` );
-      
-    }
-    
-    this.#status = status;
-    
-    return this.#postMessage( message );
-  
-  }
-  
-  postMessage( message ) {
-    
-    return this.#postMessage( message );
-    
-  }
-  
-  throwError( error ) {
-  
-    return this.#responseStatus( "failure", error );
-    
-  }
-  
-  returnSuccess( message ) {
-  
-    return this.#responseStatus( "success", message );
-    
-  }
-  
-}
+Action.methods.main = function( request, ...values ) {
 
-self.addEventListener( "message", event => {
-  
-  console.log( event );
-
-  const Request = new WorkerRequest( event.data );
-  
-  try {
-  
-    console.time( `requestID=${ Request.requestID }` );
+  for ( const value of values ) {
     
-    for ( const arg of Request.args ) {
-      
-      Request.postMessage( arg );
-      
-    }
-    
-    Request.returnSuccess();
-    return;
-    
-  } catch ( error ) {
-  
-    Request.throwError( error );
-    return;
-    
-  } finally {
-  
-    console.timeEnd( `requestID=${ Request.requestID }` );
+    request.postMessage( arg );
     
   }
   
-} );
+};
