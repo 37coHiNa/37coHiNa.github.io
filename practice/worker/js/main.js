999b2a56e8b84bc6e7282a3081dd198af3f4c2fe
37coHiNa <37cohina@gmail.com>
2020-11-27 21:30:25 +0900

    Update main.js

--- practice/worker/js/main.js
+++ practice/worker/js/main.js
@@ -1,5 +1,9 @@
 import * as MyWorker from "../js/lib/MyWorker.js";
 
 const worker = new MyWorker.Worker( "./js/worker.js", { type: "module" } );
+(async ()=>{
+  for await ( const message of worker.postMessage( "main", "test message", "test message2", 123 ) ) {
+    console.log( message );
+  }
+})();
 window.myWorker = worker;
-//worker.postMessage( "main", "test message", "test message2", 123 );
