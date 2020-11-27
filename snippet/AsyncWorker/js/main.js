697f4024f5e5cb045e369ba4c3aeea0ef9e75059
37coHiNa <37cohina@gmail.com>
2020-11-27 18:26:40 +0900

    Update main.js

--- snippet/AsyncWorker/js/main.js
+++ snippet/AsyncWorker/js/main.js
@@ -1,10 +1,15 @@
 import * as Lib from "../js/lib/PromiseWorker.js";
 
-const worker = new Lib.PromiseWorker( "../js/worker.js", { type: "module" } );
+const worker = new Lib.PromiseWorker( "./js/worker.js" );
 
 (async () => {
+  
   const values = worker.postMessage( 1, 2, 3 );
+  
   for await ( const value of values ) {
+    
     console.log( value );
+  
   }
+  
 })();
