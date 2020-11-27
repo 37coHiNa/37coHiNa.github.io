27ae43c13b7f4a824fb2af18d43c0dc6f83f0327
37coHiNa <37cohina@gmail.com>
2020-11-27 21:21:25 +0900

    Update main.js

--- practice/worker/js/main.js
+++ practice/worker/js/main.js
@@ -1,4 +1,5 @@
 import * as MyWorker from "../js/lib/MyWorker.js";
 
 const worker = new MyWorker.Worker( "./js/worker.js", { type: "module" } );
-worker.postMessage( "main", "test message", "test message2", 123 );
+window.myWorker = worker;
+//worker.postMessage( "main", "test message", "test message2", 123 );
