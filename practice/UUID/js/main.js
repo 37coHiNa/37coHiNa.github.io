71aa0087cc09c9a05e7d649ecac9c7f89b0137bd
37coHiNa <37cohina@gmail.com>
2020-12-02 06:40:47 +0900

    Update main.js

--- practice/UUID/js/main.js
+++ practice/UUID/js/main.js
@@ -1,120 +1,75 @@
-
-class UUID_origin {
-
-  static #uuidIte = ( function* () {
-
-    //RFC 4122
-    const HEXOCTETS = Object.freeze( [ ...Array(256) ].map( ( e, i ) => i.toString( 16 ).padStart( 2, "0" ).toUpperCase() ) );
-    const VARSION = 0x40;
-    const VARIANT = 0x80;
-
-    for (;;) {
-
-      const s0 = Math.random() * 0x100000000 >>> 0;
-      const s1 = Math.random() * 0x100000000 >>> 0;
-      const s2 = Math.random() * 0x100000000 >>> 0;
-      const s3 = Math.random() * 0x100000000 >>> 0;
-      yield "" +
-        HEXOCTETS[ s0 & 0xff ] +
-        HEXOCTETS[ s0 >>> 8 & 0xff ] +
-        HEXOCTETS[ s0 >>> 16 & 0xff ] +
-        HEXOCTETS[ s0 >>> 24 & 0xff ] + "-" +
-        HEXOCTETS[ s1 & 0xff ] +
-        HEXOCTETS[ s1 >>> 8 & 0xff ] + "-" +
-        HEXOCTETS[ s1 >>> 16 & 0x0f | VARSION ] +
-        HEXOCTETS[ s1 >>> 24 & 0xff ] + "-" +
-        HEXOCTETS[ s2 & 0x3f | VARIANT ] +
-        HEXOCTETS[ s2 >>> 8 & 0xff ] + "-" +
-        HEXOCTETS[ s2 >>> 16 & 0xff ] +
-        HEXOCTETS[ s2 >>> 24 & 0xff ] +
-        HEXOCTETS[ s3 & 0xff ] +
-        HEXOCTETS[ s3 >>> 8 & 0xff ] +
-        HEXOCTETS[ s3 >>> 16 & 0xff ] +
-        HEXOCTETS[ s3 >>> 24 & 0xff ];
-
-    }
-
-  } )();
+class UUID {
   
-  static randomUUID() {
+  #data = new ArrayBuffer( 16 );
   
-    return this.#uuidIte.next().value;
-  
+  constructor( obj ) {
+    
+    const data = this.#data;
+    const bytes = new Uint8Array( obj );
+    
+    Object.assign(
+      new Uint8Array( data ),
+      bytes
+    );
+    
   }
 
-}
+  #most;
+  #least;
+  #setBits() {
 
-class UUID_crypto_spread {
+    if ( this.#most != null || this.#least != null ) return;
 
-  static #uuidIte = ( function* () {
+    [ this.#most, this.#least ] = new BigInt64( this.#data );
 
-    //RFC 4122
-    const HEXOCTETS = Object.freeze( [ ...Array(256) ].map( ( e, i ) => i.toString( 16 ).padStart( 2, "0" ).toUpperCase() ) );
-    const VARSION = 0x40;
-    const VARIANT = 0x80;
-
-    for (;;) {
-
-      const [ s0, s1, s2, s3 ] = crypto.getRandomValues( new Uint32Array(4) );
-      yield "" +
-        HEXOCTETS[ s0 & 0xff ] +
-        HEXOCTETS[ s0 >>> 8 & 0xff ] +
-        HEXOCTETS[ s0 >>> 16 & 0xff ] +
-        HEXOCTETS[ s0 >>> 24 & 0xff ] + "-" +
-        HEXOCTETS[ s1 & 0xff ] +
-        HEXOCTETS[ s1 >>> 8 & 0xff ] + "-" +
-        HEXOCTETS[ s1 >>> 16 & 0x0f | VARSION ] +
-        HEXOCTETS[ s1 >>> 24 & 0xff ] + "-" +
-        HEXOCTETS[ s2 & 0x3f | VARIANT ] +
-        HEXOCTETS[ s2 >>> 8 & 0xff ] + "-" +
-        HEXOCTETS[ s2 >>> 16 & 0xff ] +
-        HEXOCTETS[ s2 >>> 24 & 0xff ] +
-        HEXOCTETS[ s3 & 0xff ] +
-        HEXOCTETS[ s3 >>> 8 & 0xff ] +
-        HEXOCTETS[ s3 >>> 16 & 0xff ] +
-        HEXOCTETS[ s3 >>> 24 & 0xff ];
-
-    }
-
-  } )();
-
-  static randomUUID() {
-  
-    return this.#uuidIte.next().value;
-  
   }
 
-}
+  get mostSignificantBits() { this.#setBits(); return this.#most; }
 
-class UUID_crypto_typed_array {
+  get leastSignificantBits() { this.#setBits(); return this.#least; }
 
+  static HEXOCTETS = Object.freeze( [ ...Array(256) ].map( ( e, i ) => i.toString( 16 ).padStart( 2, "0" ).toUpperCase() ) );
+  #stringRepresentation;
+
+  toString() {
+
+    if ( this.#stringRepresentation != null ) return this.#stringRepresentation;
+
+    const bytes = new Uint8Array( this.#data );
+    const HEXOCTETS = this.constructor.HEXOCTETS;
+
+    this.#stringRepresentation = "" +
+      HEXOCTETS[ bytes[ 0 ] ] +
+      HEXOCTETS[ bytes[ 1 ] ] +
+      HEXOCTETS[ bytes[ 2 ] ] +
+      HEXOCTETS[ bytes[ 3 ] ] + "-" +
+      HEXOCTETS[ bytes[ 4 ] ] +
+      HEXOCTETS[ bytes[ 5 ] ] + "-" +
+      HEXOCTETS[ bytes[ 6 ] ] +
+      HEXOCTETS[ bytes[ 7 ] ] + "-" +
+      HEXOCTETS[ bytes[ 8 ] ] +
+      HEXOCTETS[ bytes[ 9 ] ] + "-" +
+      HEXOCTETS[ bytes[ 10 ] ] +
+      HEXOCTETS[ bytes[ 11 ] ] +
+      HEXOCTETS[ bytes[ 12 ] ] +
+      HEXOCTETS[ bytes[ 13 ] ] +
+      HEXOCTETS[ bytes[ 14 ] ] +
+      HEXOCTETS[ bytes[ 15 ] ];
+
+  }
+  
   static #uuidIte = ( function* () {
 
     //RFC 4122
-    const HEXOCTETS = Object.freeze( [ ...Array(256) ].map( ( e, i ) => i.toString( 16 ).padStart( 2, "0" ).toUpperCase() ) );
     const VARSION = 0x40;
     const VARIANT = 0x80;
 
     for (;;) {
 
       const bytes = crypto.getRandomValues( new Uint8Array( 16 ) );
-      yield "" +
-        HEXOCTETS[ bytes[ 0 ] ] +
-        HEXOCTETS[ bytes[ 1 ] ] +
-        HEXOCTETS[ bytes[ 2 ] ] +
-        HEXOCTETS[ bytes[ 3 ] ] + "-" +
-        HEXOCTETS[ bytes[ 4 ] ] +
-        HEXOCTETS[ bytes[ 5 ] ] + "-" +
-        HEXOCTETS[ bytes[ 6 ] & 0x0f | VARSION ] +
-        HEXOCTETS[ bytes[ 7 ] ] + "-" +
-        HEXOCTETS[ bytes[ 8 ] & 0x3f | VARIANT ] +
-        HEXOCTETS[ bytes[ 9 ] ] + "-" +
-        HEXOCTETS[ bytes[ 10 ] ] +
-        HEXOCTETS[ bytes[ 11 ] ] +
-        HEXOCTETS[ bytes[ 12 ] ] +
-        HEXOCTETS[ bytes[ 13 ] ] +
-        HEXOCTETS[ bytes[ 14 ] ] +
-        HEXOCTETS[ bytes[ 15 ] ];
+      bytes[ 6 ] = bytes[ 6 ] & 0x0f | VARSION;
+      bytes[ 8 ] = bytes[ 8 ] & 0x3f | VARIANT;
+      yield new this( bytes.buffer );
 
     }
 
@@ -126,100 +81,9 @@ class UUID_crypto_typed_array {
   
   }
 
-}
-
-class UUID_crypto_bigint64 {
-
-  static #uuidIte = ( function* () {
-
-    //RFC 4122
-    const HEXOCTETS = Object.freeze( [ ...Array(256) ].map( ( e, i ) => i.toString( 16 ).padStart( 2, "0" ).toUpperCase() ) );
-    const VARSION = 0x40n;
-    const VARIANT = 0x80n;
-
-    for (;;) {
-
-      const random128 = new BigUint64Array( crypto.getRandomValues( new Int8Array( 16 ) ).buffer );
-      yield "" +
-        HEXOCTETS[ random128[0] & 0xffn ] +
-        HEXOCTETS[ random128[0] >> 8n & 0xffn ] +
-        HEXOCTETS[ random128[0] >> 16n & 0xffn ] +
-        HEXOCTETS[ random128[0] >> 24n & 0xffn ] + "-" +
-        HEXOCTETS[ random128[0] >> 32n & 0xffn ] +
-        HEXOCTETS[ random128[0] >> 40n & 0xffn ] + "-" +
-        HEXOCTETS[ random128[0] >> 48n & 0x0fn | VARSION ] +
-        HEXOCTETS[ random128[0] >> 56n & 0xffn ] + "-" +
-        HEXOCTETS[ random128[1] & 0x3fn | VARIANT ] +
-        HEXOCTETS[ random128[1] >> 8n & 0xffn ] + "-" +
-        HEXOCTETS[ random128[1] >> 16n & 0xffn ] +
-        HEXOCTETS[ random128[1] >> 24n & 0xffn ] +
-        HEXOCTETS[ random128[1] >> 32n & 0xffn ] +
-        HEXOCTETS[ random128[1] >> 40n & 0xffn ] +
-        HEXOCTETS[ random128[1] >> 48n & 0xffn ] +
-        HEXOCTETS[ random128[1] >> 56n & 0xffn ];
-
-    }
-
-  } )();
-
-  static randomUUID() {
-  
-    return this.#uuidIte.next().value;
-  
+  static fromString() {
+    //TODO
   }
 
 }
 
-class UUID_crypto_bigint {
-
-  static #uuidIte = ( function* () {
-
-    //RFC 4122
-    const HEXOCTETS = Object.freeze( [ ...Array(256) ].map( ( e, i ) => i.toString( 16 ).padStart( 2, "0" ).toUpperCase() ) );
-    const VARSION = 0x40n;
-    const VARIANT = 0x80n;
-
-    for (;;) {
-
-      const [ most, least ] = new BigUint64Array( crypto.getRandomValues( new Int8Array( 16 ) ).buffer );
-      yield "" +
-        HEXOCTETS[ most & 0xffn ] +
-        HEXOCTETS[ most >> 8n & 0xffn ] +
-        HEXOCTETS[ most >> 16n & 0xffn ] +
-        HEXOCTETS[ most >> 24n & 0xffn ] + "-" +
-        HEXOCTETS[ most >> 32n & 0xffn ] +
-        HEXOCTETS[ most >> 40n & 0xffn ] + "-" +
-        HEXOCTETS[ most >> 48n & 0x0fn | VARSION ] +
-        HEXOCTETS[ most >> 56n & 0xffn ] + "-" +
-        HEXOCTETS[ least & 0x3fn | VARIANT ] +
-        HEXOCTETS[ least >> 8n & 0xffn ] + "-" +
-        HEXOCTETS[ least >> 16n & 0xffn ] +
-        HEXOCTETS[ least >> 24n & 0xffn ] +
-        HEXOCTETS[ least >> 32n & 0xffn ] +
-        HEXOCTETS[ least >> 40n & 0xffn ] +
-        HEXOCTETS[ least >> 48n & 0xffn ] +
-        HEXOCTETS[ least >> 56n & 0xffn ];
-
-    }
-
-  } )();
-
-  static randomUUID() {
-  
-    return this.#uuidIte.next().value;
-  
-  }
-
-}
-
-for ( const target of [ UUID_origin, UUID_crypto_spread, UUID_crypto_typed_array, UUID_crypto_bigint64, UUID_crypto_bigint ] ) {
-  for ( const times of Array( 10 ).fill( 1 ).map( ( v, i ) => v + i ) ) {
-    const timeKey = `${ times }: ${ target.name }`;
-    const uuids = [];
-    console.time( timeKey );
-    for ( let i = 0; i < 100000; i++ ) {
-      uuids.push( target.randomUUID() );
-    }
-    console.timeEnd( timeKey );
-  }
-}
